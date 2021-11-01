26d950c2698a2f36c8128b7eb4a1d7119c86a6a5
Yana <Korchinskaj@gmail.com>
2021-11-01 16:40:32 +0200

    home

---
+++ projects/homework/ifelse.py
@@ -0,0 +1,9 @@
+a =input()
+b =input()
+с =input()
+if a>b :
+   print (c)
+elif a<b :
+    print ('грустный текст')
+else:
+     print ('веселый текст')
---
+++ projects/homework/randomishe.py
@@ -0,0 +1,7 @@
+import random
+
+a = [0,1,2,3,4,5,6,7,8,9]
+
+random.shuffle(a)
+
+print(a)
