f74221d6f8526f081b5a8fcc2c19eb1472071c6a
snoppy14 <93482027+snoppy14@users.noreply.github.com>
2021-11-01 15:51:15 +0200

    Delete pythonProject directory

--- pythonProject/Конкатенация.py
+++
@@ -1,7 +0,0 @@
-welcome = input('введите преветствие ')
-name = input('введите ваше имя ')
-city = input('введите ваш город ')
-
-result = welcome + " меня зовут " + name + ". я из города " + city + "!"
-
-print(result)
